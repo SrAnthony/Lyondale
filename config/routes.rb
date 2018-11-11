a5c9abb5431fe0a8f123c2859ce0f080d9e6ef64
Anthony Nadaletti <anthony.nadaletti@gmail.com>
2018-11-11 18:32:55 -0200

    Add DashboardController e rota

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'dashboard#index'
+
   resources :rents
   resources :properties
   resources :customers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
