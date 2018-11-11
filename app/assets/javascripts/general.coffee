d1eb82a9bf9e0c2f244b0ae526dc60f97ac2232d
Anthony Nadaletti <anthony.nadaletti@gmail.com>
2018-11-11 21:22:29 -0200

    Melhorias para o header

--- app/assets/javascripts/general.coffee
+++ app/assets/javascripts/general.coffee
@@ -1,2 +1,6 @@
 $(document).on 'turbolinks:load', ->
+  # === Ativa niceScroll
   $('body').niceScroll()
+
+  # === Ativa todos os dropdowns da página
+  $('.ui.dropdown').dropdown()
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  # Define a classe usada nas abas do header
+  def menu_nav_link_class(name)
+    "nav-button #{'active' if controller_name == name}"
+  end
 end
