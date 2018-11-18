e4ed0ef51b6a4c9f522bf28aee3b84d869d3e67b
Anthony Nadaletti <anthony.nadaletti@gmail.com>
2018-11-17 22:11:17 -0200

    i18n pt-BR

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,19 @@ module Lyondale
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Usa SASS ao invés de SCSS
     config.sass.preferred_syntax = :sass
+
+    # Adiciona classe em inputs com erros de validação
+    ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+      class_attr_index = html_tag.index 'class="'
+
+      return html_tag.insert class_attr_index+7, 'field-error ' if class_attr_index
+      html_tag.insert html_tag.index('>'), ' class="field-error"'
+    end
+
+    # i18n
+    config.i18n.default_locale = :'pt-BR'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
