3bc75a3a06d1a953611750fce0f01d9ea4ca0288
Anthony Nadaletti <anthony.nadaletti@gmail.com>
2018-11-18 14:55:07 -0200

    Reduz limite de itens na dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,8 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @customers = Customer.order(created_at: :desc).limit(5)
-    @properties = Property.order(created_at: :desc).limit(5)
+    @customers = Customer.order(created_at: :desc).limit(3)
+    @properties = Property.order(created_at: :desc).limit(3)
     @rents = Rent.order(created_at: :desc).limit(5)
   end
 end
